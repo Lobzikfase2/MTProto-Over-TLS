de6bf13bbe09a2d8c948cbb563818852611f54b0
Lobzikfase2 <86695758+Lobzikfase2@users.noreply.github.com>
2026-02-14 06:13:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -115,20 +115,40 @@ configure_ufw() {
 # Настраиваем ядро
 configure_sysctl() {
   log "Конфигурация ядра"
+  # Убираем базовые настройки, делая из них бэкап
+  if [ -f /etc/sysctl.conf ]; then
+      sudo mv /etc/sysctl.conf /etc/sysctl.conf.bak
+      echo "✅ /etc/sysctl.conf переименован в .bak"
+  else
+      echo "ℹ️ /etc/sysctl.conf не найден, пропускаю создание бэкапа"
+  fi
   sudo tee /etc/sysctl.d/99-tuning.conf > /dev/null <<EOF
-net.ipv4.tcp_fastopen = 0
-# net.ipv4.tcp_timestamps -- Спорно
-# 0 - дает меньше данных для анализа
-# 1 - делает больше похожим на https
-# GPT настаивает не выключать
 net.ipv4.tcp_timestamps = 1
+net.ipv4.tcp_fastopen = 1
+net.ipv4.tcp_rfc1337 = 0
+
+# Производительность
 net.ipv4.tcp_congestion_control = bbr
 net.core.default_qdisc = fq
+net.ipv4.tcp_mtu_probing = 1
 net.core.somaxconn = 4096
 net.core.netdev_max_backlog = 16384
-net.ipv4.tcp_mtu_probing = 1
-net.ipv4.tcp_rfc1337 = 1
+
+# Хуже не будет (пригодится для VLESS)
+net.ipv4.ip_local_port_range = 10240 65535
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+net.ipv4.tcp_rmem = 4096 87380 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
+
+# Хуже не будет (но полезен только для UDP)
+net.ipv4.udp_rmem_min = 8192
+net.ipv4.udp_wmem_min = 8192
+
 net.ipv4.conf.all.rp_filter = 1
+net.ipv4.tcp_syncookies = 1
+
+# Нужно лишь для telemt
 net.ipv4.ip_unprivileged_port_start = 443
 EOF
   sudo sysctl --system
