d8c3d92367c64475da294fbb7f34c24da8abcc10
Lobzikfase2 <86695758+Lobzikfase2@users.noreply.github.com>
2026-02-14 23:31:44 +0300

    Comment out configure_ufw and configure_sysctl
    Comment out UFW and sysctl configuration calls in install script.

--- install.sh
+++ install.sh
@@ -300,8 +300,8 @@ main() {
     fi
 
     install_components
-    configure_ufw
-    configure_sysctl
+    # configure_ufw
+    # configure_sysctl
 
     log "Клонирование репозитория"
     rm -rf $USER_HOME/mtp_proxy && mkdir -p $USER_HOME/mtp_proxy && cd $USER_HOME/mtp_proxy
